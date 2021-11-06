00ba3268932ac8bf5eb069bbbad74c7803e2fd52
murali elumalai <muralismail4u@gmail.com>
2021-11-06 13:33:53 +0530

    🐛 FIX:

--- components/templates/Team/Team.js
+++ components/templates/Team/Team.js
@@ -3,7 +3,7 @@ function Team() {
     <div className="container px-5 py-24 mx-auto">
       <div className="flex flex-col text-center w-full mb-20">
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably havent heard of them.</p>
       </div>
       <div className="flex flex-wrap -m-2">
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
--- components/templates/Testimonials/testimonials.js
+++ components/templates/Testimonials/testimonials.js
@@ -13,12 +13,12 @@ function Testimonial() {
           </svg>
           <p className="leading-relaxed text-lg">
             Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki
-            taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman
+            taxidermy 90s cronut +1 kinfolk. Single-origin coffee ennui shaman
             taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid
             fanny pack vaporware. Man bun next level coloring book skateboard
             four loko knausgaard. Kitsch keffiyeh master cleanse direct trade
             indigo juice before they sold out gentrify plaid gastropub normcore
-            XOXO 90's pickled cindigo jean shorts. Slow-carb next level
+            XOXO 90s pickled cindigo jean shorts. Slow-carb next level
             shoindigoitch ethical authentic, yr scenester sriracha forage
             franzen organic drinking vinegar.
           </p>
